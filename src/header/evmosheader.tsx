812947b0fee9d987cc9a7105bde488c4492bcb9a
alpha-omega-labs <88520218+alpha-omega-labs@users.noreply.github.com>
2021-12-30 22:39:53 -0500

    Update evmosheader.tsx

--- src/header/evmosheader.tsx
+++ src/header/evmosheader.tsx
@@ -18,7 +18,7 @@ const EvmosHeader = () => {
         <Box py={12} w="full" boxShadow="lg" bgColor="teal.600">
             <VStack spacing={3} alignItems="center">
                 <Heading size="3xl" color="white">
-                    Evmos.me
+                    genesisL1
                 </Heading>
                 <Text color="white">Your all in one evmos wallet</Text>
                 <WalletButtons />
